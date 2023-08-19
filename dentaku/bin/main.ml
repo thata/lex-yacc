29399e9bb39a8681d135eb90ab24920809c58a55
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2023-08-19 22:47:38 +0900

    evalとmainを実装

--- dentaku/bin/main.ml
+++ dentaku/bin/main.ml
@@ -1,1 +1,6 @@
-let () = print_endline "Hello, World!"
+let () =
+  let buff = Lexing.from_channel stdin in
+  let expr = Dentaku.Parser.start Dentaku.Lexer.token(buff) in
+  let result = Dentaku.Eval.f expr in
+  print_int result;
+  print_newline ()
---
+++ dentaku/lib/eval.ml
@@ -0,0 +1,9 @@
+let rec f expr =
+  match expr with
+    Syntax.Num(n) -> n
+  | Syntax.Op(e1, op, e2) ->
+      let v1 = f e1 in
+      let v2 = f e2 in
+      match op with
+        Syntax.Minus -> v1 - v2
+      | Syntax.Times -> v1 * v2
