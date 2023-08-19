812a282175af3a638ad8c79ad63e90f6ba2f8ad8
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2023-08-19 22:57:36 +0900

    dune fmt

--- dentaku/bin/main.ml
+++ dentaku/bin/main.ml
@@ -1,6 +1,6 @@
 let () =
   let buff = Lexing.from_channel stdin in
-  let expr = Dentaku.Parser.start Dentaku.Lexer.token(buff) in
+  let expr = Dentaku.Parser.start Dentaku.Lexer.token buff in
   let result = Dentaku.Eval.f expr in
   print_int result;
   print_newline ()
--- dentaku/lib/eval.ml
+++ dentaku/lib/eval.ml
@@ -1,9 +1,7 @@
 let rec f expr =
   match expr with
-    Syntax.Num(n) -> n
-  | Syntax.Op(e1, op, e2) ->
+  | Syntax.Num n -> n
+  | Syntax.Op (e1, op, e2) -> (
       let v1 = f e1 in
       let v2 = f e2 in
-      match op with
-        Syntax.Minus -> v1 - v2
-      | Syntax.Times -> v1 * v2
+      match op with Syntax.Minus -> v1 - v2 | Syntax.Times -> v1 * v2)
